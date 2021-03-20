562ff0b1113b6dad1af4c9d6e65946653f209805
KOUASSI CHARLES OTH-NIEL <kouassicharlesothniel@gmail.com>
2021-03-20 07:29:06 +0000

    add firebase

--- Login/Login.js
+++ Login/Login.js
@@ -1,72 +1,137 @@
-import React from 'react'
+import React, {useState} from 'react';
 import {
     View,
+    Text,
     StyleSheet,
-    ScrollView,
-    Image
+    TextInput,
+    Image,
+    ActivityIndicator
 } from 'react-native';
-import Header from './Header'
-import Username from './Username'
-import Password from './Password'
-import ButtonConnect from './ButtonConnect'
-import MotDePasse from './MotDePasse'
-import Contacter from './Contacter';
+import * as firebase from 'firebase';
+import { Container, Header, Form, Input, Item, Button, Label } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
 import delivery from '../assets/images/food-delivery.png'
 
 
-const Login = () => {
-    const [text, setText] = React.useState('');
+const firebaseConfig = {
+    apiKey: "AIzaSyCqkqjOgHBODPLYf-ZhQ7RcSZoK9C1Chro",
+    authDomain: "payauth-ab60f.firebaseapp.com",
+    projectId: "payauth-ab60f",
+    storageBucket: "payauth-ab60f.appspot.com",
+    messagingSenderId: "271742196676",
+    appId: "1:271742196676:web:012d3a7b9a21e5330f6291"
+  };
+  // Initialize Firebase
+  if(!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+  }
 
-    return (
-        <ScrollView style={styles.scrollView}>
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Header />
-                </View>
-                <View style={styles.other}>
-                    <Username onChangeText={() => setText()} />
-                    <Password />
-                    <MotDePasse />
-                    <Contacter />
-                    <ButtonConnect />
-                </View>
-                <View style={styles.logo}>
+const Login = () => {
+
+    const navigation = useNavigation();
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const signUp = (email, password) => {
+        console.log('password', password)
+        try {
+            if(password.length<5){
+                alert('Entrer 5 caractères svp')
+                return;
+            }
+            
+            // alert(email)
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            setEmail('')
+            setPassword('')
+        } catch (error) {
+            console.log(error.toString())
+        }
+    }
+
+    const login = (email, password) => {
+        try {
+            if(email.length<5){
+                alert('Entrer 5 caractères svp')
+                return;
+            }
+            
+            firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((users) => {
+                console.log(users.user)
+                // setLoading(true)
+                navigation.navigate("ShoppingPage")
+                
+            })
+        } catch (error) {
+            alert('inscrivez vous')
+        }
+    }
+
+    return(
+        <Container style={styles.container}>
+            <Form>
+           <View style={{paddingLeft:10}}>
+               <TextInput 
+                onChangeText={(email) => setEmail(email)}
+                placeholder="Email"
+                autoCorrect={false}
+                autoCapitalize='none'
+               />
+               <View style={{borderBottomColor:'black',borderBottomWidth:1}}></View>
+           </View>
+            <View style={{paddingLeft:10}}>
+               <TextInput 
+                onChangeText={(password) => setPassword(password)}
+                placeholder="Password"
+                secureTextEntry={true}
+                autoCorrect={false}
+                autoCapitalize='none'
+               />
+               <View style={{borderBottomColor:'black',borderBottomWidth:1}}></View>
+           </View>
+                <Button 
+                onPress={() => login(email, password)}
+                style={{marginTop:40}}
+                full rounded success>
+                    <Text style={{color:'white'}}>Login</Text>
+                </Button>
+
+                <Button 
+                onPress={() => signUp(email, password)}
+                style={{marginTop:10}}
+                full rounded warning>
+                    <Text style={{color:'white'}}>Sign Up</Text>
+                </Button>
+            </Form>
+            <View style={styles.logo}>
                     <Image
                         style={styles.logoPay}
                         source={delivery}
                     />
                 </View>
-            </View>
-        </ScrollView>
-    );
-};
-
-
-export default Login;
+        </Container> 
+    )
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff'
-    },
-    scrollView: {
-        backgroundColor: 'white',
-    },
-    header: {
-        flex: 1,
-        justifyContent: 'space-around',
-        alignItems: 'center',
+    container:{
+        flex:1,
+        backgroundColor:'white',
+        justifyContent:'center',
+        padding:10
     },
     logo: {
         paddingVertical: 20,
         paddingHorizontal: 25,
         alignItems: 'center',
-        marginTop: 40,
+        marginBottom: -150,
     },
     logoPay: {
         width: 100,
         height: 100,
     },
-
-
 });
+
+export default Login;
